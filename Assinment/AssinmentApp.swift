1ee019fe1a58dbec8e069b92a5a9ab4b7811e4ab
Swapnil Tilkari <swapniltilkari7@gmail.com>
2022-04-25 15:46:27 +0530

    First Commit

---
+++ Assinment/AssinmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssinmentApp.swift
+//  Assinment
+//
+//  Created by Swapnil Tilkari on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AssinmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assinment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assinment
+//
+//  Created by Swapnil Tilkari on 21/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      
+        ///
+        /// MARK: change url here
+        CustomImageView(url:"https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/Samsung/M53_launch/M53/M53V1/M53V2/M53_14apr/53_landingpage/landingpage_pc/Camera_1500xh.gif")
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
